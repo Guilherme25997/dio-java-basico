6bcbc3ee5210c335b5a2bacea22b9419d6f3e15b
Guilherme25997 <guilhermeandradeg25@gmail.com>
2025-05-06 16:21:57 -0300

    teste 2

--- Meu-primeiro-projeto-java/src/App.java
+++ Meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas Vinda!W");
+        System.out.println("Boas Vinda!Wfd");
     }
 }
